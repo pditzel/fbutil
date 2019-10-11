58baf75e48ae1a9aed9c3dcb6c2112c602731c1f
Patrick Ditzel <patrick@localhost.localdomain>
2019-10-11 15:02:44 +0200

    initial commit!

---
+++ README.md
@@ -0,0 +1,15 @@
+## fbutil
+
+
+### Beschreibung
+
+Bash-Skript zum Steuern und Abfragen einer Fritzbox
+
+### Erweiterungen
+
+Module sind bash-Funktionen die in einzelnen Dateien im
+"lib/modules"-Verzeichniss liegen. Jedes Modul hat eine Konfigurationsdatei im
+Verzeichnis "fbutil.cfg.d"mit dem selben Namen wie das Modul. Erst wenn eine
+Konfigurationsdatei angelegt ist wird das Modul geladen.
+
+Die Steuerung erfolgt ausschließlich über die Konfigurationsdateien.
---
+++ bin/fbutil.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+################################################################################
+#                                                                              #
+#    Author: Patrick Ditzel (patrick@central-computer.de)                      #
+#                                                                              #
+#    License: GPLv3                                                            #
+#                                                                              #
+################################################################################
+
+main () {
+
+  #----------------------------------------------------------------------------#
+  # Bibliotheken einbinden
+  for LIB in $(find ./lib -maxdepth 1 -type f -printf '%f\n'); do
+    # Debugging-Ausgabe
+    #echo "lib to include: $LIB"\
+    #| /usr/bin/logger -s -i -t pshome
+    source ./lib/"${LIB}"
+  done
+
+  #----------------------------------------------------------------------------#
+
+  # Konfiguration einlesen
+  setConfig
+
+  #----------------------------------------------------------------------------#
+
+  # Session starten und SessionID bereitstellen
+  FBSID=$(getFBSID)
+
+  #============================================================================#
+
+  # Module ausführen
+
+    #
+    # Nutzbare Variablen:
+    # ${FRITZBOXFQDN}     -   Die Adresse der Firtzbox
+    # ${FBSID}            -   Die aktuell gültige SessionID
+    #
+
+  # Alle Devices an der FB ausgeben
+  fbshdevinfo
+
+  # Backup FRITZBOX
+  fbbackupcfg
+
+  # Capture FB NetworDeviceTraffic
+  fbcapture
+
+
+  #============================================================================#
+
+
+
+
+  # Session beenden
+  closeSession "$FBSID"
+
+  #----------------------------------------------------------------------------#
+
+}
+
+main
